b1a7cd74c1e4061301851ac603aa611de63d09be
kumahacker <kumahavana@gmail.com>
2018-01-31 19:16:42 -0500

    restric to use username and not email

--- service.php
+++ service.php
@@ -15,8 +15,7 @@ class Credito extends Service
 		$receiver = false;
 
 		// do not allow blank searches
-		if(empty($request->query))
-		{
+		if (empty($request->query)) {
 			// get the person's credit
 			$person = $utils->getPerson($request->email);
 			$credit = number_format($person->credit, 2);
@@ -29,48 +28,42 @@ class Credito extends Service
 				ORDER BY transfer.transfer_time DESC
 				LIMIT 0,50;";
 
-			$connection = new Connection();
-			$r = $connection->query($sql);
+			$r = Connection::query($sql);
 			if (!isset($r[0])) $r = false;
 
 			$response = new Response();
 			$response->setResponseSubject("Su credito");
-			$response->createFromTemplate("home.tpl", array("credit"=>$credit, "items" => $r));
+			$response->createFromTemplate("home.tpl", ["credit" => $credit, "items" => $r]);
 
 			return $response;
 		}
 
-		// get the email and the amount to send
-		foreach(explode(" ", $request->query) as $value)
-		{
-			// check if it is a valid @username or email
-			$temp = $utils->getEmailFromUsername($value);
-			if($temp) $value = $temp;
-			if(filter_var($value, FILTER_VALIDATE_EMAIL)) $receiver = $value;
+		// clear query
+		$request->query = trim($request->query);
+		while (stripos($request->query, '  ')) $request->query = str_replace('  ', ' ', $request->query);
+		$arr = explode(' ', $request->query);
 
-			// check if it is a valid money amount
-			$number = str_replace(",", ".", $value);
-			if(preg_match("/^-?[0-9]+(?:\.[0-9]{1,2})?$/", $number)) $amount = $number;
-		}
+		if (isset($arr[0])) $receiver = $arr[0];
+		if (isset($arr[1])) $amount = $arr[1] * 1;
 
 		// return error response if the receiver or the amount are wrong
-		if(empty($amount) || empty($receiver))
-		{
-			if(empty($receiver)) $message = "El email o @username de la persona a recibir no es correcto. Puede que usted halla escrito el email mal por error.";
-			else $message = "La cantidad insertada no es correcta, parece que usted inserto un n&uacute;mero que no es v&aacute;lido.";
+		if (empty($amount) || empty($receiver)) {
+			if (empty($receiver)) $message = "El @username de la persona a recibir no es correcto. Verifiquelo.";
+			else $message = "La cantidad insertada no es correcta, parece que usted insert&oacute; un n&uacute;mero que no es v&aacute;lido.";
 
 			// send response to the user
-			$responseContent = array("message" => $message, "query" => $request->query);
+			$responseContent = ["message" => $message, "query" => $request->query];
 			$response = new Response();
-			$response->subject = "El email o la cantidad a transferir son incorrectas";
+			$response->subject = "El nombre de usuario o la cantidad a transferir son incorrectas";
 			$response->createFromTemplate("invalid.tpl", $responseContent);
 			return $response;
 		}
 
+		$receiverEmail = $utils->getEmailFromUsername($receiver);
+
 		// check if the person exist. If not, message the requestor
-		if( ! $utils->personExist($receiver))
-		{
-			$responseContent = array("email" => $receiver);
+		if (!$utils->personExist($receiverEmail)) {
+			$responseContent = ["email" => $receiver];
 			$response = new Response();
 			$response->subject = "La persona no existe";
 			$response->createFromTemplate("inexistent.tpl", $responseContent);
@@ -79,16 +72,14 @@ class Credito extends Service
 
 		// check if you have enough credit to transfer
 		$profile = $utils->getPerson($request->email);
-		if($profile->credit < $amount)
-		{
+		if ($profile->credit < $amount) {
 			// send response to the user
-			$responseContent = array("amount" => $amount, "credit" => $profile->credit, "email" => $receiver);
+			$responseContent = ["amount" => $amount, "credit" => $profile->credit, "email" => $receiver];
 			$template = "nocredit.tpl";
 
-			if($request->subject == "PURCHASE")
-			{
+			if ($request->subject == "PURCHASE") {
 				$template = "nocreditPurchase.tpl";
-				$responseContent = array_merge($responseContent, array("nameOfItemToPurchase" => $request->body));
+				$responseContent = array_merge($responseContent, ["nameOfItemToPurchase" => $request->body]);
 			}
 
 			$response = new Response();
@@ -100,18 +91,15 @@ class Credito extends Service
 		// save the transfer intention in the database
 		$confirmationHash = $utils->generateRandomHash();
 		$inventory_code = $request->subject == "PURCHASE" ? $request->name : "NULL";
-		$query = "INSERT INTO transfer(sender,receiver,amount,confirmation_hash,inventory_code) VALUES ('{$request->email}', '$receiver', '$amount', '$confirmationHash', '$inventory_code')";
-		$connection = new Connection();
-		$connection->query($query);
+		Connection::query("INSERT INTO transfer(sender,receiver,amount,confirmation_hash,inventory_code) VALUES ('{$request->email}', '$receiverEmail', '$amount', '$confirmationHash', '$inventory_code')");
 
 		// create the variables for the view
 		$template = "confirmation.tpl";
-		$responseContent = array("amount" => $amount, "receiver" => $this->utils->getUsernameFromEmail($receiver), "hash" => $confirmationHash);
+		$responseContent = ["amount" => $amount, "receiver" => "@$receiver", "hash" => $confirmationHash];
 
-		if($request->subject == "PURCHASE")
-		{
+		if ($request->subject == "PURCHASE") {
 			$template = "confirmPurchase.tpl";
-			$responseContent = array_merge($responseContent, array("nameOfItemToPurchase" => $request->body));
+			$responseContent = array_merge($responseContent, ["nameOfItemToPurchase" => $request->body]);
 		}
 
 		// email the confirmation to transfer the credits
@@ -132,12 +120,10 @@ class Credito extends Service
 		$hash = $request->query;
 
 		// get the transfer details, ensure the transfer is valid
-		$connection = new Connection();
-		$transfer = $connection->query("SELECT * FROM transfer WHERE confirmation_hash = '$hash' && transfered = 0");
+		$transfer = Connection::query("SELECT * FROM transfer WHERE confirmation_hash = '$hash' && transfered = 0");
 
 		// error if the hash was not valid or the transaction was used already
-		if(empty($hash) || empty($transfer))
-		{
+		if (empty($hash) || empty($transfer)) {
 			$response = new Response();
 			$response->subject = "Error procesando su transferencia o pago";
 			$response->createFromText("Tuvimos un error procesando su transferencia, o puede que esta transferencia ya se halla cobrado. Su cr&eacute;dito no ha sido afectado. Por favor intente nuevamente.");
@@ -150,11 +136,10 @@ class Credito extends Service
 		$elapsedTimeInHours = $seconds / 60 / 60;
 
 		// error if the transfer is out of date
-		if($elapsedTimeInHours > 1)
-		{
+		if ($elapsedTimeInHours > 1) {
 			$response = new Response();
 			$response->subject = "Su transferencia o pago ha expirado";
-			$responseContent = array("amount" => $transferRow->amount, "receiver" => $this->utils->getUsernameFromEmail($transferRow->receiver));
+			$responseContent = ["amount" => $transferRow->amount, "receiver" => $this->utils->getUsernameFromEmail($transferRow->receiver)];
 			$response->createFromTemplate("expired.tpl", $responseContent);
 			return $response;
 		}
@@ -162,10 +147,9 @@ class Credito extends Service
 		// check if you still have enough credit to transfer
 		$utils = new Utils();
 		$profile = $utils->getPerson($transferRow->sender);
-		if($profile->credit < $transferRow->amount)
-		{
+		if ($profile->credit < $transferRow->amount) {
 			// send response to the user
-			$responseContent = array("amount" => $transferRow->amount, "credit" => $profile->credit, "email" => $this->utils->getUsernameFromEmail($transferRow->receiver));
+			$responseContent = ["amount" => $transferRow->amount, "credit" => $profile->credit, "email" => $this->utils->getUsernameFromEmail($transferRow->receiver)];
 			$response = new Response();
 			$response->subject = "No tiene suficiente credito";
 			$response->createFromTemplate("nocredit.tpl", $responseContent);
@@ -173,7 +157,7 @@ class Credito extends Service
 		}
 
 		// make the transfer
-		$connection->query("
+		Connection::query("
 			START TRANSACTION;
 			UPDATE person SET credit=credit-{$transferRow->amount} WHERE email='{$request->email}';
 			UPDATE person SET credit=credit+{$transferRow->amount} WHERE email='{$transferRow->receiver}';
@@ -181,17 +165,14 @@ class Credito extends Service
 			COMMIT;");
 
 		// if it is a transfer
-		if($transferRow->inventory_code == "NULL")
-		{
+		if ($transferRow->inventory_code == "NULL") {
 			$template = "receipt.tpl";
 			$itemBought = ""; // empty for transfers
 			$subject = "Su transferencia ha sido realizada correctamente";
-		}
-		// if it is a sell, execute the selling code
-		else
-		{
+		} // if it is a sell, execute the selling code
+		else {
 			// get the transfer row
-			$inventory = $connection->query("SELECT * FROM inventory WHERE code = '{$transferRow->inventory_code}'")[0];
+			$inventory = Connection::query("SELECT * FROM inventory WHERE code = '{$transferRow->inventory_code}'")[0];
 			$serviceName = strtolower($inventory->service);
 
 			// include the service
@@ -199,8 +180,7 @@ class Credito extends Service
 			$object = new $serviceName();
 
 			// if the object has a method payment
-			if(method_exists($object, "payment"))
-			{
+			if (method_exists($object, "payment")) {
 				// create the payment object
 				$payment = new Payment();
 				$payment->code = $inventory->code;
@@ -223,11 +203,11 @@ class Credito extends Service
 		$this->utils->addNotification($transferRow->receiver, 'Credito', $msg, 'CREDITO', 'IMPORTANT');
 
 		// create response to send to the user
-		$responseContent = array(
+		$responseContent = [
 			"amount" => $transferRow->amount,
 			"receiver" => $this->utils->getUsernameFromEmail($transferRow->receiver),
 			"itemBought" => $itemBought // used only for payment
-		);
+		];
 
 		// send the receipt to the sender
 		$response = new Response();
@@ -247,12 +227,10 @@ class Credito extends Service
 		$code = $request->query;
 
 		// get the payment details
-		$connection = new Connection();
-		$inventory = $connection->query("SELECT * FROM inventory WHERE code = '$code' && active = 1");
+		$inventory = Connection::query("SELECT * FROM inventory WHERE code = '$code' && active = 1");
 
 		// error if the code was not valid or the inventory item cannot be found or its not active
-		if(empty($code) || empty($inventory))
-		{
+		if (empty($code) || empty($inventory)) {
 			$article = empty($code) ? "" : ", $code, ";
 			$response = new Response();
 			$response->subject = "Articulo incorrecto o temporalmente agotado";
