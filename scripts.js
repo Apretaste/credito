117d3fb8c8d11fe361341f96b1688610f8f6e392
kumahacker <kumahavana@gmail.com>
2019-08-17 18:45:05 -0400

    inventory name

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-//
-// formats a date
-//
+function pad(n, width, z) {
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
 function formatDate(dateStr) {
 	var date = new Date(dateStr);
 	var year = date.getFullYear();
-	var month = (1 + date.getMonth()).toString().padStart(2, '0');
-	var day = date.getDate().toString().padStart(2, '0');
+	var month = pad(1 + date.getMonth(),2);
+	var day = pad(date.getDay(),2);
 	var hour = (date.getHours() < 12) ? date.getHours() : date.getHours() - 12;
 	var minutes = date.getMinutes();
 	var amOrPm = (date.getHours() < 12) ? "am" : "pm";
