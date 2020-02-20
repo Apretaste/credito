68eea4a7b3dace948ff111f4239772c6c17c6fbb
salvipascual <salvi.pascual@gmail.com>
2020-02-20 18:33:56 +0100

    fix transfer button

--- service.php
+++ service.php
@@ -10,9 +10,9 @@ use Apretaste\Level;
 class Service
 {
 	/**
-	 * Main function
+	 * Display the list of transactions
 	 */
-	public function _main(Request $request, Response &$response)
+	public function _main(Request $request, Response $response)
 	{
 		// get all transfers
 		$transfers = Money::transactions($request->person->id);
@@ -31,9 +31,9 @@ class Service
 
 		// create response data
 		$content = [
-				'credit' => $request->person->credit,
-				'items' => $transfers,
-				'canTransfer' => $request->person->level >= Level::TOPACIO,
+			'credit' => $request->person->credit,
+			'items' => $transfers,
+			'canTransfer' => $request->person->levelCode >= Level::TOPACIO
 		];
 
 		// send response
@@ -43,38 +43,36 @@ class Service
 	/**
 	 * Show the list of ways to obtain credits
 	 *
-	 * @param Request
-	 * @param Response
-	 *
-	 * @throws \Exception
+	 * @param Request $request
+	 * @param Response $response
 	 */
-	public function _obtener(Request $request, Response &$response)
+	public function _obtener(Request $request, Response $response)
 	{
+		// complete the challenge
+		Challenges::complete('read-how-to-obtain-credit', $request->person->id);
+
+		// send data to the view
 		$response->setCache('month');
 		$response->setTemplate('obtain.ejs');
 
-		Challenges::complete('read-how-to-obtain-credit', $request->person->id);
 	}
 
 	/**
 	 * Starts a new transfer
 	 *
-	 * @param \Apretaste\Request $request
-	 * @param \Apretaste\Response $response
-	 *
-	 * @throws \Framework\Alert
+	 * @param Request $request
+	 * @param Response $response
 	 */
-	public function _enviar(Request $request, Response &$response)
+	public function _enviar(Request $request, Response $response)
 	{
 		// error if you do not have enought level to transfer
 		if ($request->person->level < Level::TOPACIO) {
-			$response->setTemplate('message.ejs', [
-					'header' => 'Nivel insuficiente',
-					'icon' => 'sentiment_very_dissatisfied',
-					'text' => '¡Hola! Usted aún no es nivel Topacio, por lo cual no podrá realizar una tranferencia de crédito. Siga usando la app para subir de nivel.',
-					'button' => ['href' => 'PERFIL NIVELES', 'caption' => 'Ver mi nivel']
+			return $response->setTemplate('message.ejs', [
+				'header' => 'Nivel insuficiente',
+				'icon' => 'sentiment_very_dissatisfied',
+				'text' => '¡Hola! Usted aún no es nivel Topacio, por lo cual no podrá realizar una tranferencia de crédito. Siga usando la app para subir de nivel.',
+				'button' => ['href' => 'PERFIL NIVELES', 'caption' => 'Ver mi nivel']
 			]);
-			return;
 		}
 
 		$response->setCache('year');
@@ -84,23 +82,19 @@ class Service
 	/**
 	 * Execute a transfer
 	 *
-	 * @param \Apretaste\Request $request
-	 * @param \Apretaste\Response $response
-	 *
-	 * @return void
-	 * @throws \Framework\Alert
+	 * @param Request $request
+	 * @param Response $response
 	 */
-	public function _transfer(Request $request, Response &$response)
+	public function _transfer(Request $request, Response $response)
 	{
 		// error if you do not have enought level to transfer
 		if ($request->person->level < Level::TOPACIO) {
-			$response->setTemplate('message.ejs', [
-					'header' => 'Nivel insuficiente',
-					'icon' => 'sentiment_very_dissatisfied',
-					'text' => '¡Hola! Usted aún no es nivel Topacio, por lo cual no podrá realizar una tranferencia de crédito. Siga usando la app para subir de nivel.',
-					'button' => ['href' => 'PERFIL NIVELES', 'caption' => 'Ver mi nivel']
+			return $response->setTemplate('message.ejs', [
+				'header' => 'Nivel insuficiente',
+				'icon' => 'sentiment_very_dissatisfied',
+				'text' => '¡Hola! Usted aún no es nivel Topacio, por lo cual no podrá realizar una tranferencia de crédito. Siga usando la app para subir de nivel.',
+				'button' => ['href' => 'PERFIL NIVELES', 'caption' => 'Ver mi nivel']
 			]);
-			return;
 		}
 
 		// get params for the transfer
@@ -112,34 +106,32 @@ class Service
 		$person = Person::find($username);
 
 		if ($person === false) {
-			$response->setTemplate('message.ejs', [
-					'header' => 'Usuario no encontrado',
-					'icon' => 'sentiment_very_dissatisfied',
-					'text' => 'El usuario al cual quiere transferir no existe en el sistema. Por favor intente nuevamente.',
-					'button' => ['href' => 'CREDITO ENVIAR', 'caption' => 'Transferir']
+			return $response->setTemplate('message.ejs', [
+				'header' => 'Usuario no encontrado',
+				'icon' => 'sentiment_very_dissatisfied',
+				'text' => 'El usuario al cual quiere transferir no existe en el sistema. Por favor intente nuevamente.',
+				'button' => ['href' => 'CREDITO ENVIAR', 'caption' => 'Transferir']
 			]);
-			return;
 		}
 
 		// send the transfer
 		try {
 			Money::send($request->person->id, $person->id, $amount, $reason);
 		} catch (Exception $e) {
-			$response->setTemplate('message.ejs', [
-					'header' => 'Error inesperado',
-					'icon' => 'sentiment_very_dissatisfied',
-					'text' => 'Encontramos un error inesperado transfiriendo su crédito. Por favor intente nuevamente.',
-					'button' => ['href' => 'CREDITO ENVIAR', 'caption' => 'Transferir']
+			return $response->setTemplate('message.ejs', [
+				'header' => 'Error inesperado',
+				'icon' => 'sentiment_very_dissatisfied',
+				'text' => 'Encontramos un error inesperado transfiriendo su crédito. Por favor intente nuevamente.',
+				'button' => ['href' => 'CREDITO ENVIAR', 'caption' => 'Transferir']
 			]);
-			return;
 		}
 
 		// return ok message
 		$response->setTemplate('message.ejs', [
-				'header' => 'Crédito enviado',
-				'icon' => 'pan_tool',
-				'text' => "¡Chócala! Usted ha enviado §$amount a @{$person->username} correctamente. Esta transfencia se mostrará en sus transacciones.",
-				'button' => ['href' => 'CREDITO', 'caption' => 'Transacciones']
+			'header' => 'Crédito enviado',
+			'icon' => 'pan_tool',
+			'text' => "¡Chócala! Usted ha enviado §$amount a @{$person->username} correctamente. Esta transfencia se mostrará en sus transacciones.",
+			'button' => ['href' => 'CREDITO', 'caption' => 'Transacciones']
 		]);
 	}
 }
