97a1df709e20ce807f48bdeb67bcc503f5d78787
salvipascual <salvi.pascual@gmail.com>
2020-04-20 11:07:43 -0400

    remove credit transfers

--- service.php
+++ service.php
@@ -31,9 +31,8 @@ class Service
 
 		// create response data
 		$content = [
-			'credit' => $request->person->credit,
-			'items' => $transfers,
-			'canTransfer' => $request->person->levelCode >= Level::TOPACIO
+			'credit' => number_format($request->person->credit, 2),
+			'items' => $transfers
 		];
 
 		// send response
@@ -55,83 +54,5 @@ class Service
 		$response->setCache('month');
 		$response->setTemplate('obtain.ejs');
 
-	}
-
-	/**
-	 * Starts a new transfer
-	 *
-	 * @param Request $request
-	 * @param Response $response
-	 */
-	public function _enviar(Request $request, Response $response)
-	{
-		// error if you do not have enought level to transfer
-		if ($request->person->levelCode < Level::TOPACIO) {
-			return $response->setTemplate('message.ejs', [
-				'header' => 'Nivel insuficiente',
-				'icon' => 'sentiment_very_dissatisfied',
-				'text' => '¡Hola! Usted aún no es nivel Topacio, por lo cual no podrá realizar una tranferencia de crédito. Siga usando la app para subir de nivel.',
-				'button' => ['href' => 'PERFIL NIVELES', 'caption' => 'Ver mi nivel']
-			]);
-		}
-
-		$response->setCache('year');
-		$response->setTemplate('enviar.ejs', ['credit' => $request->person->credit]);
-	}
-
-	/**
-	 * Execute a transfer
-	 *
-	 * @param Request $request
-	 * @param Response $response
-	 */
-	public function _transfer(Request $request, Response $response)
-	{
-		// error if you do not have enought level to transfer
-		if ($request->person->levelCode < Level::TOPACIO) {
-			return $response->setTemplate('message.ejs', [
-				'header' => 'Nivel insuficiente',
-				'icon' => 'sentiment_very_dissatisfied',
-				'text' => '¡Hola! Usted aún no es nivel Topacio, por lo cual no podrá realizar una tranferencia de crédito. Siga usando la app para subir de nivel.',
-				'button' => ['href' => 'PERFIL NIVELES', 'caption' => 'Ver mi nivel']
-			]);
-		}
-
-		// get params for the transfer
-		$amount = (float) $request->input->data->price;
-		$username = $request->input->data->username;
-		$reason = $request->input->data->reason;
-
-		// get the person who will receive the funds
-		$person = Person::find($username);
-
-		if ($person === false) {
-			return $response->setTemplate('message.ejs', [
-				'header' => 'Usuario no encontrado',
-				'icon' => 'sentiment_very_dissatisfied',
-				'text' => 'El usuario al cual quiere transferir no existe en el sistema. Por favor intente nuevamente.',
-				'button' => ['href' => 'CREDITO ENVIAR', 'caption' => 'Transferir']
-			]);
-		}
-
-		// send the transfer
-		try {
-			Money::send($request->person->id, $person->id, $amount, $reason);
-		} catch (Exception $e) {
-			return $response->setTemplate('message.ejs', [
-				'header' => 'Error inesperado',
-				'icon' => 'sentiment_very_dissatisfied',
-				'text' => 'Encontramos un error inesperado transfiriendo su crédito. Por favor intente nuevamente.',
-				'button' => ['href' => 'CREDITO ENVIAR', 'caption' => 'Transferir']
-			]);
-		}
-
-		// return ok message
-		$response->setTemplate('message.ejs', [
-			'header' => 'Crédito enviado',
-			'icon' => 'pan_tool',
-			'text' => "¡Chócala! Usted ha enviado §$amount a @{$person->username} correctamente. Esta transfencia se mostrará en sus transacciones.",
-			'button' => ['href' => 'CREDITO', 'caption' => 'Transacciones']
-		]);
 	}
 }
